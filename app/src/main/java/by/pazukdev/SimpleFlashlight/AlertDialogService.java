e50688e5f3ff5ae7b05c5899a6574ce9d238db8e
pazukdev <pazukdev@gmail.com>
2018-06-11 13:42:03 +0300

    Initial commit

---
+++ app/src/main/java/by/pazukdev/SimpleFlashlight/AlertDialogService.java
@@ -0,0 +1,123 @@
+package by.pazukdev.SimpleFlashlight;
+
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by PazukDev on 11.06.2018.
+ */
+
+public class AlertDialogService {
+
+    private Context context;
+    private AlertDialog alertDialog;
+
+
+    public AlertDialogService(Context context) {
+        this.context = context;
+    }
+
+
+    public AlertDialog getAlertDialog(String alertDialogType) {
+
+        alertDialog = new AlertDialog.Builder(new ContextThemeWrapper(context,
+                R.style.MyAlertDialog)).create();
+
+        alertDialog.setCancelable(true);
+
+        if(alertDialogType.equals("Privacy Policy")) createPrivacyPolicyDialog();
+        if(alertDialogType.equals("App start error")) createAppStartErrorDialog();
+        if(alertDialogType.equals("Camera start error")) createCameraStartErrorDialog();
+
+        alertDialog.show();
+
+        styleDialog(); // style alert dialog to app style
+
+        return alertDialog;
+    }
+
+
+    private void createPrivacyPolicyDialog() {
+        alertDialog.setCancelable(false);
+        alertDialog.setTitle(context.getResources().getString(R.string.privacy_policy_button));
+        alertDialog.setTitle(context.getResources().getString(R.string.privacy_policy_button));
+        alertDialog.setIcon(R.drawable.ic_launcher);
+        alertDialog.setMessage(context.getResources().getString(R.string.privacy_policy));
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE,
+                context.getResources().getString(R.string.close),
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+    }
+
+
+    private void createAppStartErrorDialog() {
+        alertDialog.setTitle(context.getResources().getString(R.string.app_start_error_title));
+        alertDialog.setMessage(context.getResources().getString(R.string.app_start_error_message));
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE,
+                context.getResources().getString(R.string.close_app), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ((AppCompatActivity)context).finish();
+                    }
+                });
+
+        alertDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialogInterface) {
+                Toast.makeText(context, context.getResources()
+                                .getString(R.string.alertdialog_oncancel_toast_text), Toast.LENGTH_LONG).show();
+                alertDialog.show();
+            }
+        });
+    }
+
+
+    private void createCameraStartErrorDialog() {
+        alertDialog.setTitle(context.getResources().getString(R.string.camera_start_error_title));
+        alertDialog.setMessage(context.getResources().getString(R.string.camera_statr_error_message));
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE,
+                context.getResources().getString(R.string.close_app), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ((AppCompatActivity)context).finish();
+                    }
+                });
+        alertDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialogInterface) {
+                Toast.makeText(context, context.getResources()
+                        .getString(R.string.alertdialog_oncancel_toast_text), Toast.LENGTH_LONG).show();
+                alertDialog.show();
+            }
+        });
+    }
+
+
+    private void styleDialog() {
+
+        // set button and button text colors
+        Button button = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        button.setTextColor(ContextCompat.getColor(context, R.color.colorPrimaryDark));
+        button.setBackgroundColor(ContextCompat.getColor(context, R.color.colorTextLight));
+
+        // set title divider color
+        int titleDividerId = context.getResources().getIdentifier("titleDivider", "id", "android");
+        View titleDivider = alertDialog.findViewById(titleDividerId);
+        if (titleDivider != null)
+            titleDivider.setBackgroundColor(ContextCompat.getColor(context, R.color.colorPrimary));
+
+    }
+
+}
---
+++ app/src/main/java/by/pazukdev/SimpleFlashlight/MainActivity.java
@@ -0,0 +1,118 @@
+package by.pazukdev.SimpleFlashlight;
+
+
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import java.util.List;
+
+/**
+ * Created by PazukDev on 01.04.2017.
+ *
+ * Updated to 1.03 version on 11.06.2018
+ *
+ */
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean hasFlash;
+    private Camera camera; // deprecated class Camera will be changed to CameraDevice at one of
+                           // the next updates if it increase min Android version up to 21
+    private Parameters parameters;
+
+    private Button button;
+
+    private AlertDialogService alertDialogService = new AlertDialogService(this);
+
+
+    private void hasFlashCheck() {
+        hasFlash = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if(!hasFlash) alertDialogService.getAlertDialog("App start error");
+        else {
+            try {
+                camera = Camera.open();
+            }
+            catch (RuntimeException e) {
+                e.printStackTrace();
+                Log.e("Error, can't start: ", e.getMessage());
+                alertDialogService.getAlertDialog("Camera start error");
+            }
+        }
+
+    }
+
+    private void getCamera() {
+        try {
+            if(camera != null) parameters = camera.getParameters();
+        }
+        catch (RuntimeException e) {
+            e.printStackTrace();
+            Log.e("Error, can't start: ", e.getMessage());
+            alertDialogService.getAlertDialog("Camera start error");
+        }
+    }
+
+    private void flashOn() {
+        if (parameters != null) {
+            List supportedFlashModes = parameters.getSupportedFlashModes();
+            if(supportedFlashModes.contains(Parameters.FLASH_MODE_TORCH)) {
+                parameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+            }
+            else if(supportedFlashModes.contains(Parameters.FLASH_MODE_ON)) {
+                parameters.setFlashMode(Parameters.FLASH_MODE_OFF);
+            }
+            else camera = null;
+            if(camera != null) {
+                camera.setParameters(parameters);
+                camera.startPreview();
+            }
+        }
+    }
+
+    private void  flashOff() {
+        parameters = camera.getParameters();
+        parameters.setFlashMode(Parameters.FLASH_MODE_OFF);
+        camera.setParameters(parameters);
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+
+        hasFlashCheck();
+        getCamera();
+        flashOn();
+
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialogService.getAlertDialog("Privacy Policy");
+
+                // uncomment for test
+                //alertDialogService.getAlertDialog("App start error");
+                //alertDialogService.getAlertDialog("Camera start error");
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        flashOff();
+        super.onDestroy();
+
+    }
+}
